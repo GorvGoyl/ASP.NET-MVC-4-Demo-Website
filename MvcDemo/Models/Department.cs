fc92f84f6eba6e1a650e9409ca42fcbc4fb44636
jerrygoyal <1gouravgg@gmail.com>
2015-08-17 16:00:21 +0530

    added sample database of employees and departmnt

--- MvcDemo/Models/Department.cs
+++ MvcDemo/Models/Department.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations.Schema;
 namespace MvcDemo.Models
 {
+    [Table("tblDepartment")]
     public class Department
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
     }
 }
